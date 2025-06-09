ea4ce51b9e9bebf70f478d1cf6e71b582efe9819
webbyrasika <rvtembhare2605@gmail.com>
2025-06-09 20:18:05 +0530

    new changes in jre file

--- E2SeleniumProject/src/Locators/ClassNameLocator1.java
+++ E2SeleniumProject/src/Locators/ClassNameLocator1.java
@@ -1,21 +1,21 @@
+
+
+
 package Locators;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ClassNameLocator1 {
-
+public class ClassNameLocator1
+{
 	public static void main(String[] args)
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
 		driver.get("https://demowebshop.tricentis.com/login");
-		
-		driver.findElement(By.className("login-button")).click();
-		
+		driver.findElement(By.className("login-button")).click();	
 
 	}
-
+	
 }
